541d6a4fe4132e9abeceb1a38a635f1707dc7fc1
anhtuan1410 <anhtuantran1410@gmail.com>
2021-07-01 17:13:36 +0700

    Add project files.

---
+++ SignalClient/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SignalClient
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConnectHubSignalR();
+        }
+
+        void ConnectHubSignalR()
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _hub.Invoke("DetermineLength", "Hello").Wait();
+        }
+
+        public void DetermineLength(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+
+        public async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            __connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await _connection.StartAsync();
+                messagesList.Items.Add("_connection started");
+                connectButton.IsEnabled = false;
+                sendButton.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        public async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await _connection.InvokeAsync("SendMessage",
+                    userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ SignalWPF/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignalWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:44377/chathub")
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+        }
+
+        private async void connectButton_Click(object sender, RoutedEventArgs e)
+        {
+            //ReceiveMessage this is a name of event when Clients.All.SendAsync("ReceiveMessage", user, message)
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    var newMessage = $"{user}: {message}";
+                    messagesList.Items.Add(newMessage);
+                });
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Items.Add("Connection started");
+                btnConnect.IsEnabled = false;
+                btnSendMsg.IsEnabled = true;
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+        private async void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //SendMessage this is a name of event, of method in your hub on server
+                await connection.InvokeAsync("SendMessage",
+                    userTextBox.Text, messageTextBox.Text);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Items.Add(ex.Message);
+            }
+        }
+
+
+
+    }
+}
